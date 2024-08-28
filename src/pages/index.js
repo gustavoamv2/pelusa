c3cf3e6fdb2bdf03f871f9d57e4a2b08a087a105
Gustavo <gustavoamv2@gmail.com>
2024-08-28 15:21:33 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import dbConnect from "../lib/dbConnect";
 import Pet from "../models/Pet";
 
 // TODO: Import Hours component
-
+import Hours from '../components/Hours';
 const Index = ({ pets }) => {
 
   return (
     <>
 
       {/* TODO: Display Hours component */}
+    <Hours />
 import Hours from '../components/Hours';
 <Hours />
       {/* Create a card for each pet */}
