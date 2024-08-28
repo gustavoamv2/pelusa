75514b5eb736752047086dfa7441f8271d4d6f74
Gustavo <gustavoamv2@gmail.com>
2024-08-28 15:08:48 -0400

    Update Hours.js

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,16 @@
 // Placeholder to host a component
+import React from 'react';
+
+const Hours = () => {
+  const date = new Date();
+  const hours = date.getHours();
+
+  return (
+    <div>
+      <h2>Hora actual del día:</h2>
+      <p>{hours}:00</p>
+    </div>
+  );
+};
+
+export default Hours;
